3042f65a7a0c1d8331ad8bb65a6b0db3c8587703
Ishaan Kumar <ffishaan@gmail.com>
2019-03-26 16:35:00 -0400

    moved scripts inside dir

--- maskrcnn-benchmark/train_maskrcnn.sh
+++ maskrcnn-benchmark/train_maskrcnn.sh
@@ -1,5 +1,5 @@
 export CUDA_HOME=/cvmfs/soft.computecanada.ca/easybuild/software/2017/avx/Compiler/intel2016.4/cuda/9.0.176
 export LD_LIBRARY_PATH=/cvmfs/soft.computecanada.ca/easybuild/software/2017/avx/Compiler/intel2016.4/cuda/9.0.176/lib64/:$LD_LIBRARY_PATH
 source activate humanware
-python maskrcnn-benchmark/tools/train_net.py \
-    --config-file maskrcnn-benchmark/configs/humanware_e2e_faster_rcnn_R_101_FPN_1x.yaml
+python tools/train_net.py \
+    --config-file configs/humanware_e2e_faster_rcnn_R_101_FPN_1x.yaml
