8571f2a08561377641a12f42f1de0ee2e24c1399
Ishaan Kumar <ffishaan@gmail.com>
2019-03-26 16:11:33 -0400

    corrected pbs script

--- train_maskrcnn.sh
+++ train_maskrcnn.sh
@@ -1,23 +1,5 @@
-
-#!/bin/bash
-#PBS -S /bin/bash
-#PBS -N TACHE       # Nom de la tâche
-#PBS -A colosse-users  # Identifiant Rap; ID
-#PBS -l feature=k80
-#PBS -l walltime=8:00:00    # Durée en secondes
-#PBS -l nodes=1:gpus=1  # Nombre de noeuds.
-# do not execute on login nodes
-module --force purge
-
-#XXXPBXXXS -l advres=MILA2019
-PATH=$PATH:/opt/software/singularity-3.0/bin/
-
-# set the working directory to where the job is launched
-cd "${PBS_O_WORKDIR}"
-
-# Singularity options 
-IMAGE=/rap/jvb-000-aa/COURS2019/etudiants/ift6759.simg
-RAP=/rap/jvb-000-aa/COURS2019/etudiants/$USER
-
+export CUDA_HOME=/cvmfs/soft.computecanada.ca/easybuild/software/2017/avx/Compiler/intel2016.4/cuda/9.0.176
+export LD_LIBRARY_PATH=/cvmfs/soft.computecanada.ca/easybuild/software/2017/avx/Compiler/intel2016.4/cuda/9.0.176/lib64/:$LD_LIBRARY_PATH
 source activate humanware
-s_exec python maskrcnn-benchmark/tools/train_net.py --config-file maskrcnn-benchmark/configs/humanware_e2e_faster_rcnn_R_101_FPN_1x.yaml
+python maskrcnn-benchmark/tools/train_net.py \
+    --config-file maskrcnn-benchmark/configs/humanware_e2e_faster_rcnn_R_101_FPN_1x.yaml
