09900420fa6161eefae36299fc47a8303bb6e8f2
Ishaan Kumar <ffishaan@gmail.com>
2019-04-12 11:44:50 -0400

    Pipeline for evaluation (#9)
    * changes for test dataset and best pipeline
    
    * imported eval scripts
    
    * converter that generates metadata.pkl file for second stage using bbox.json
    
    * corrected bloc3_evaluation.sh
    
    * changed weight file in best config
    
    * corrected bloc3 evaluation script
    
    * minor correction in converter
    
    * permission correction
    
    * minor correction in bloc3
    
    * corrected converter code
    
    * changes for proof of concept run through
    
    * removed irrelevant comments
    
    * correct comment

---
+++ evaluation/bloc3_evaluation.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+set -e
+
+export CUDA_HOME=/cvmfs/soft.computecanada.ca/easybuild/software/2017/avx/Compiler/intel2016.4/cuda/9.0.176
+export LD_LIBRARY_PATH=/cvmfs/soft.computecanada.ca/easybuild/software/2017/avx/Compiler/intel2016.4/cuda/9.0.176/lib64/:$LD_LIBRARY_PATH
+
+CURR_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+ROOT_DIR=`cd $CURR_DIR/../ && pwd`
+MASKRCNN_DIR=$ROOT_DIR/"maskrcnn-benchmark"
+EVAL_DIR=$SECOND_STAGE_CODE/evaluation
+INFERENCE_DIR=$MASKRCNN_DIR/inference/coco_humanware_test
+
+TEST_DIR=$MASKRCNN_DIR/datasets/test_dir
+TEST_INSTANCES=$TEST_DIR/instances_test.json
+
+SECOND_STAGE_CODE=$ROOT_DIR/b2phut2/code
+BEST_RCNN_MODEL=$ROOT_DIR/saved_models/"r_101_batch_size=2_iter=14400.pth"
+BBOX_FILE=$INFERENCE_DIR/bbox.json
+METADATA_FILENAME=$INFERENCE_DIR/metadata.pkl
+
+function check_error(){
+    exit_code=`echo $?`
+    if [ $exit_code -ne 0 ]; then
+        echo "$1"
+        exit $exit_code
+    fi
+}
+
+
+## Activate the conda environment
+source activate humanware
+
+### FILL OUT THE REST OF THE FILE ###
+
+## build maskrcnn-bechnmark ##
+echo "compiling maskrcnn..."
+compile_stm=`cd $MASKRCNN_DIR && python setup.py build develop`
+check_error "***** Unable to compile maskrcnn *****"
+
+## run maskrcnn on test data ##
+
+# cleanup stale files
+rm -rf $INFERENCE_DIR \
+    $TEST_DIR \
+    $MASKRCNN_DIR/model_final.pth \
+    $MASKRCNN_DIR/last_checkpoint
+
+# set up soft link to DATA_DIR
+ln -s $DATA_DIR $TEST_DIR
+
+# set up soft link to best model
+ln -s $BEST_RCNN_MODEL $MASKRCNN_DIR/model_final.pth
+
+echo "Running maskrcnn on test set..."
+cd $MASKRCNN_DIR
+python tools/test_net.py \
+    --config-file configs/humanware_best.yaml
+
+check_error "Unable to run maskrcnn on test set"
+
+## convert bbox.json to metadata.pkl ##
+python $CURR_DIR/converter.py --bbox-file $BBOX_FILE --instance-file $TEST_INSTANCES --output-file $METADATA_FILENAME
+check_error "Unable to generate metadata file"
+
+## run predictions with on test data with metadata.pkl ##
+cd $SECOND_STAGE_CODE/evaluation 
+python eval.py --dataset_dir=$DATA_DIR --results_dir=$RESULTS_DIR --metadata_filename=$METADATA_FILENAME
