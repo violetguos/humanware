f62b743474357fa2409965ae8f669d996179a944
Ishaan Kumar <ffishaan@gmail.com>
2019-04-14 17:51:08 -0400

    train script modifications

--- b2phut2/b3_src/train_loader_b2.sh
+++ b2phut2/b3_src/train_loader_b2.sh
@@ -4,16 +4,15 @@
 CURR_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 ROOT_DIR=`cd $CURR_DIR/../../../ && pwd`
 
-export B2_ROOT_DIR=$ROOT_DIR/humanware/b2phut2/code
-export SVHN_DIR='/rap/jvb-000-aa/COURS2019/etudiants/data/humanware/Humanware_v1_1553272293'
-export DATA_DIR=$SVHN_DIR/train
-export TMP_DATA_DIR=$DATA_DIR
-export TMP_RESULTS_DIR=$DATA_DIR
-export METADATA_FILENAME='/rap/jvb-000-aa/COURS2019/etudiants/data/humanware/Humanware_v1_1553272293/train/avenue_train_metadata_split.pkl'
-
+B2_ROOT_DIR=$ROOT_DIR/humanware/b2phut2/code
+SVHN_DIR='/rap/jvb-000-aa/COURS2019/etudiants/data/humanware/Humanware_v1_1553272293'
+TRAIN_DATA_DIR=$SVHN_DIR/train
+TRAIN_METADATA_FILENAME=$TRAIN_DATA_DIR/'avenue_train_metadata_split.pkl'
+VALIDATION_DIR=$SVHN_DIR/valid
+VALIDATION_METADATA_FILENAME=$VALIDATION_DIR/'avenue_valid_metadata_split.pkl'
 source /rap/jvb-000-aa/COURS2019/etudiants/common.env
 
 PYTHONPATH=$B2_ROOT_DIR python -u $B2_ROOT_DIR/train_loader_new_data.py \
-    --dataset_dir=$DATA_DIR --metadata_filename=$METADATA_FILENAME \
-    --results_dir=$HOME/humanware/results --cfg=$B2_ROOT_DIR/config/retrain_b2.yaml \
-    --model=$ROOT_DIR/humanware/saved_models/block2_model/best_model.pth
+    --dataset_dir=$TRAIN_DATA_DIR --metadata_filename=$TRAIN_METADATA_FILENAME \
+    --results_dir=$ROOT_DIR/humanware/results --cfg=$B2_ROOT_DIR/config/tune_smart.yaml \
+    --extra_dataset_dir=$VALIDATION_DIR --extra_metadata_filename=$VALIDATION_METADATA_FILENAME
