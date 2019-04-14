f2eb50f2d4c73a1a4130ef8e18ab19e73a1673da
Ishaan Kumar <ffishaan@gmail.com>
2019-04-14 18:48:56 -0400

    appropriate name for fine tuning scripts

---
+++ scripts/fine_tune_b2.sh
@@ -0,0 +1,15 @@
+ #!/bin/bash
+
+CURR_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+ROOT_DIR=`cd $CURR_DIR/../ && pwd`
+
+B2_ROOT_DIR=$ROOT_DIR/src
+SVHN_DIR='/rap/jvb-000-aa/COURS2019/etudiants/data/humanware/Humanware_v1_1553272293'
+DATA_DIR=$SVHN_DIR/train
+METADATA_FILENAME=$SVHN_DIR/'avenue_train_metadata_split.pkl'
+
+source /rap/jvb-000-aa/COURS2019/etudiants/common.env
+
+PYTHONPATH=$B2_ROOT_DIR python -u $B2_ROOT_DIR/train.py \
+    --dataset_dir=$DATA_DIR --metadata_filename=$METADATA_FILENAME \
+    --results_dir=$ROOT_DIR/results --cfg=$B2_ROOT_DIR/config/retrain_b2.yaml
